11018f1a82bbc37181b39e3ab5dc39df38d218a3
njokuifeanyigerald <brainboyrichmond@gmail.com>
2023-04-14 17:50:04 +0100

    debugging

--- script.groovy
+++ script.groovy
@@ -51,7 +51,7 @@ def ecrDockerPush(String aws_account_id, String region, String ecr_repoName){
 }
 
 
-def ecrDockerPush(String aws_account_id, String region, String ecr_repoName){
+def ecrDockerImageRemove(String aws_account_id, String region, String ecr_repoName){
     sh """
         docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
        """
