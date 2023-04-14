c02e82036c5a8213cae99f7f6b9b2db221a5371b
njokuifeanyigerald <brainboyrichmond@gmail.com>
2023-04-14 17:21:05 +0100

    debugging

--- script.groovy
+++ script.groovy
@@ -38,7 +38,6 @@ def dockerImageRemove(String project, String ImageTag, String hubUser){
 
 def ecrDockerBuild(String aws_account_id, String region, String ecr_repoName){
     sh """
-        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
         docker build -t ${ecr_repoName} .
         docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
        """
