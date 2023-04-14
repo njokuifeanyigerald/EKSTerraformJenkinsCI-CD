5c36785640337e0b3327bc2530091272ce96b063
njokuifeanyigerald <brainboyrichmond@gmail.com>
2023-04-14 06:13:03 +0100

    added dockerbuild with groovy script

--- script.groovy
+++ script.groovy
@@ -1,6 +1,6 @@
 def dockerbuild(String project, String ImageTag, String hubUser){
     sh '''
-        docker image build -t ${hubUser}/${project} .
+        docker image build -t ${hubUser}${project} .
         docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
         docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
        '''
