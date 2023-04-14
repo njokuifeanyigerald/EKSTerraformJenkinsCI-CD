c2570e65a938905180f11a58390c3cd140161381
njokuifeanyigerald <brainboyrichmond@gmail.com>
2023-04-14 18:24:02 +0100

    debugging

--- script.groovy
+++ script.groovy
@@ -51,8 +51,10 @@ def ecrDockerScan(String aws_account_id, String region, String ecr_repoName){
 }
 
 def ecrDockerPush(String aws_account_id, String region, String ecr_repoName){
+    // aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+
     sh """
-        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+        docker login -u AWS -p $(aws ecr get-login-password --region ${region}) ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
 
         docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
        """
